c94a9ec934e79e988cf0a8c550a7a397164585f2
Kristian Kristensen <kristian-kristensen@hotmail.com>
2018-09-14 11:40:39 +0200

    DAO structure

---
+++ BachelorBackEnd/DAO imp's/ParticipantDAOImp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BachelorBackEnd;
+
+namespace BachelorBackEnd
+{
+    public class ParticipantDAOImp : IParticipantDAO
+    {
+        public void AddParticipant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteParticipant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllPArticipants()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetParticipant()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BachelorBackEnd/DAO imp's/ResearcherDAOImp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BachelorBackEnd
+{
+    public class ResearcherDAOImp : IResearcherDAO
+    {
+        public void AddResearcher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteResearcher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllResearchers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetResearcher()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
